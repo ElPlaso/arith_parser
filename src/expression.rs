d4c39dd1d199496f481f29913f3e88428e5a5d76
ElPlaso <plahsoekusay@gmail.com>
2023-07-30 18:35:38 +1200

    add enums for arith ast expressions.

--- src/expression.rs
+++ src/expression.rs
@@ -1,18 +1,46 @@
 use std::fmt::{Display, Error};
 
 #[derive(PartialEq)]
-pub enum Expression{}
+pub enum Expression {
+    Integer(i64),
+    Variable(String),
+    Boolean(bool),
+    BinaryOp {
+        op: BinaryOperator,
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    UnaryOp {
+        op: UnaryOperator,
+        child: Box<Expression>,
+    },
+}
 
-impl Display for Expression{
-    fn fmt(&self, f : &mut std::fmt::Formatter<'_>) -> Result<(), Error>{
+#[derive(PartialEq)]
+pub enum BinaryOperator {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    LessThan,
+    Equals,
+    And,
+    Or,
+}
+
+#[derive(PartialEq)]
+pub enum UnaryOperator {
+    Not,
+}
+
+impl Display for Expression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), Error> {
         unimplemented!()
     }
 }
 
 impl Expression {
-
-    pub fn eval(&self) -> Result<Expression, String>{
+    pub fn eval(&self) -> Result<Expression, String> {
         unimplemented!()
     }
-
 }
