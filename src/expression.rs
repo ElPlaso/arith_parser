3c7b424367ae67b078ddd1a85b3876d29d45e6be
ElPlaso <plahsoekusay@gmail.com>
2023-07-31 19:43:46 +1200

    implement displays for the current expressions.

--- src/expression.rs
+++ src/expression.rs
@@ -35,7 +35,36 @@ pub enum UnaryOperator {
 
 impl Display for Expression {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), Error> {
-        unimplemented!()
+        match self {
+            Expression::Integer(value) => write!(f, "{}", value),
+            Expression::Variable(name) => write!(f, "{}", name),
+            Expression::Boolean(value) => write!(f, "{}", if *value { "T" } else { "F" }),
+            Expression::BinaryOp { op, lhs, rhs } => write!(f, "({} {} {})", lhs, op, rhs),
+            Expression::UnaryOp { op, child } => write!(f, "({}{})", op, child),
+        }
+    }
+}
+
+impl Display for BinaryOperator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), Error> {
+        match self {
+            BinaryOperator::Add => write!(f, "+"),
+            BinaryOperator::Subtract => write!(f, "-"),
+            BinaryOperator::Multiply => write!(f, "*"),
+            BinaryOperator::Divide => write!(f, "/"),
+            BinaryOperator::LessThan => write!(f, "<"),
+            BinaryOperator::Equals => write!(f, "="),
+            BinaryOperator::And => write!(f, "&&"),
+            BinaryOperator::Or => write!(f, "||"),
+        }
+    }
+}
+
+impl Display for UnaryOperator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), Error> {
+        match self {
+            UnaryOperator::Not => write!(f, "!"),
+        }
     }
 }
 
