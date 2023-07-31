17f0def29d6c407d5c1b54baa7b534ccc2fb09b7
ElPlaso <plahsoekusay@gmail.com>
2023-07-31 21:11:12 +1200

    test the expression display implementation.

--- src/test.rs
+++ src/test.rs
@@ -1,10 +1,89 @@
 #[cfg(test)]
+
+mod display_tests {
+    use crate::expression::{BinaryOperator, Expression, UnaryOperator};
+
+    use super::*;
+
+    #[test]
+    fn test_display_integer() {
+        let expr = Expression::Integer(42);
+        assert_eq!(format!("{}", expr), "42");
+    }
+
+    #[test]
+    fn test_display_variable() {
+        let expr = Expression::Variable("x".to_string());
+        assert_eq!(format!("{}", expr), "x");
+    }
+
+    #[test]
+    fn test_display_boolean() {
+        let expr1 = Expression::Boolean(true);
+        assert_eq!(format!("{}", expr1), "T");
+
+        let expr2 = Expression::Boolean(false);
+        assert_eq!(format!("{}", expr2), "F");
+    }
+
+    #[test]
+    fn test_display_binary_op() {
+        let expr = Expression::BinaryOp {
+            op: BinaryOperator::Add,
+            lhs: Box::new(Expression::Integer(3)),
+            rhs: Box::new(Expression::Integer(4)),
+        };
+        assert_eq!(format!("{}", expr), "(3 + 4)");
+    }
+
+    #[test]
+    fn test_display_unary_op() {
+        let expr = Expression::UnaryOp {
+            op: UnaryOperator::Not,
+            child: Box::new(Expression::Boolean(true)),
+        };
+        assert_eq!(format!("{}", expr), "(!T)");
+    }
+
+    #[test]
+    fn test_display_func() {
+        let expr = Expression::Func {
+            param: "x".to_string(),
+            body: Box::new(Expression::BinaryOp {
+                op: BinaryOperator::Multiply,
+                lhs: Box::new(Expression::Variable("x".to_string())),
+                rhs: Box::new(Expression::Integer(2)),
+            }),
+        };
+        assert_eq!(format!("{}", expr), "(fn x => (x * 2))");
+    }
+
+    #[test]
+    fn test_display_if() {
+        let expr = Expression::If {
+            condition: Box::new(Expression::Boolean(true)),
+            then_expr: Box::new(Expression::Integer(42)),
+            else_expr: Box::new(Expression::Integer(0)),
+        };
+        assert_eq!(format!("{}", expr), "(if T then 42 else 0)");
+    }
+
+    #[test]
+    fn test_display_apply() {
+        let expr = Expression::Apply {
+            func_expr: Box::new(Expression::Variable("f".to_string())),
+            arg_expr: Box::new(Expression::Integer(10)),
+        };
+        assert_eq!(format!("{}", expr), "(f 10)");
+    }
+}
+
 mod arith_tests {
     use crate::expression::Expression;
     use crate::parser::Parser;
 
     #[test]
-    fn parse_var(){
+    fn parse_var() {
         let mut prog = Parser::new(&"x");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -13,7 +92,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_int(){
+    fn parse_int() {
         let mut prog = Parser::new(&"123");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -22,7 +101,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_bool(){
+    fn parse_bool() {
         let mut prog = Parser::new(&"T");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -31,7 +110,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_plus(){
+    fn parse_plus() {
         let mut prog = Parser::new(&"+(1, 1)");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -40,7 +119,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_minus(){
+    fn parse_minus() {
         let mut prog = Parser::new(&"-(1, 1)");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -49,7 +128,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_mult(){
+    fn parse_mult() {
         let mut prog = Parser::new(&"*(1, 1)");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -58,7 +137,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_div(){
+    fn parse_div() {
         let mut prog = Parser::new(&"/(1, 1)");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -67,7 +146,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_lt(){
+    fn parse_lt() {
         let mut prog = Parser::new(&"<(1, 1)");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -76,7 +155,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_not(){
+    fn parse_not() {
         let mut prog = Parser::new(&"!T");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -85,7 +164,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_eq(){
+    fn parse_eq() {
         let mut prog = Parser::new(&"=(1, 1)");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -94,7 +173,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_func(){
+    fn parse_func() {
         let mut prog = Parser::new(&"func x => T");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -103,7 +182,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_app(){
+    fn parse_app() {
         let mut prog = Parser::new(&"apply(func x => x, 1)");
         let result = prog.parse();
         assert!(result.is_ok());
@@ -112,7 +191,7 @@ mod arith_tests {
     }
 
     #[test]
-    fn parse_if(){
+    fn parse_if() {
         let mut prog = Parser::new(&"if <(1, 5) then 8 else 9");
         let result = prog.parse();
         assert!(result.is_ok());
