e2e21cdfdbb09210fbd4a3a226ab1d44c810bbee
ElPlaso <plahsoekusay@gmail.com>
2023-08-08 13:51:20 +1200

    add simple program which asks for user input.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
-pub mod parser;
+use crate::parser::Parser;
+
 pub mod expression;
+pub mod parser;
 pub mod test;
 
 fn main() {
+    loop {
+        println!("Enter an expression to evaluate:");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        let mut prog = Parser::new(input.trim());
+        match prog.parse() {
+            Ok(parsed) => {
+                match parsed.eval() {
+                    Ok(result) => {
+                        println!("-----");
+                        println!("Problem: {}", format!("{}", &parsed));
+                        println!("Answer: {}", format!("{}", &result));
+                        println!("-----");
+                    }
+                    Err(error) => {
+                        eprintln!("Error evaluating expression: {}", error);
+                    }
+                }
+            }
+            Err(error) => {
+                eprintln!("Error parsing expression: {}", error);
+            }
+        }
+    }
 }
